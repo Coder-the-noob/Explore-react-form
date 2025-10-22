62b9b91d853857bb0710ad0d2555c8b5da943ea0
Coder-the-noob <moinmatubbar6@gmail.com>
2025-10-23 02:06:41 +0600

    added some examole using context API

--- src/Components/FamilyTree/Aunt.jsx
+++ src/Components/FamilyTree/Aunt.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { use } from 'react';
 import Cousin from './Cousin';
+import { MoneyContext } from './FamilyTree';
 
 const Aunt = ({asset}) => {
+    const [money, setMoney] = use(MoneyContext);
     return (
         <div>
             <h3>Aunt</h3>
             <section className='flex'>
                 <Cousin name="Cousin 3" asset={asset}></Cousin>
                 <Cousin name="Cousin 4"></Cousin>
+                <button onClick={() => setMoney(money + 5000)}>Add Money</button>
             </section>
         </div>
     );
--- src/Components/FamilyTree/FamilyTree.jsx
+++ src/Components/FamilyTree/FamilyTree.jsx
@@ -1,21 +1,27 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import Grandpa from "./Grandpa";
 import "./FamilyTree.css";
 
-
 // eslint-disable-next-line react-refresh/only-export-components
 export const AssetContext = createContext("");
+// eslint-disable-next-line react-refresh/only-export-components
+export const MoneyContext = createContext(0);
 
 const FamilyTree = () => {
+    const [money, setMoney] = useState(0);
+
   const asset = "diamond";
   const newAsset = "gold";
   return (
     <div className="family-tree">
       <h2>Family Tree</h2>
+      <h4>Total Family Money: {money}</h4>
 
-      <AssetContext.Provider value={newAsset}>
-        <Grandpa asset={asset}></Grandpa>
-      </AssetContext.Provider>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <AssetContext.Provider value={newAsset}>
+          <Grandpa asset={asset}></Grandpa>
+        </AssetContext.Provider>
+      </MoneyContext.Provider>
     </div>
   );
 };
